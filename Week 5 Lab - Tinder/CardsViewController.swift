08d414d31c67784d8698092dc9619566d282bd19
bhudelson <bhudelso@gmail.com>
2016-03-02 09:35:36 -0800

    Implemented pan and snap actions

--- Week 5 Lab - Tinder/CardsViewController.swift
+++ Week 5 Lab - Tinder/CardsViewController.swift
@@ -10,30 +10,57 @@ import UIKit
 
 class CardsViewController: UIViewController {
 
+    //OUTLETS
     @IBOutlet weak var navBarView: UIImageView!
     @IBOutlet weak var actionButtonView: UIImageView!
     @IBOutlet weak var cardView: UIImageView!
     
+    
+    //VARIABLES
     var cardInitialCenter: CGPoint!
     
+    var swipedRightPosition: CGFloat!
+    var snappedBackPosition: CGFloat!
+    var swipedLeftPosition: CGFloat!
     
+    var rotations : [Float] = [-10, -10, 10, 10, 10, -10]
+    
+    
+    //VIEW DID LOAD
     override func viewDidLoad() {
+        
+        snappedBackPosition = cardView.center.x
+        swipedRightPosition = cardView.center.x + 320
+        swipedLeftPosition = cardView.center.x - 320
+        
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    
+    //ON PAN CARD
     @IBAction func onPanCard(sender: UIPanGestureRecognizer) {
+        
+//        var offset = cardView.contentOffset.x
         
         print("pan now")
         
         let translation = sender.translationInView(view)
         
+//        var cardrotation = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: 10, r2Max: 0)
+        
         //Began
         if sender.state == UIGestureRecognizerState.Began {
             cardInitialCenter = cardView.center
             
-            cardView.transform = CGAffineTransformRotate(cardView.transform,CGFloat(45 * M_PI / 180))
+            UIView.animateWithDuration(0.4, delay: 0.0,
+                // Autoreverse runs the animation backwards and Repeat cycles the animation indefinitely.
+                options: [UIViewAnimationOptions.Autoreverse],animations: { () -> Void in
+                        self.cardView.transform = CGAffineTransformMakeRotation(CGFloat(45 * M_PI / 180))
+                }, completion: nil)
+            
             
             
         }
@@ -42,8 +69,35 @@ class CardsViewController: UIViewController {
         else if sender.state == UIGestureRecognizerState.Changed {
             self.cardView.center = CGPoint(x: translation.x + cardInitialCenter.x, y: cardInitialCenter.y)
     }
+        
+            //Ended
+        else if sender.state == UIGestureRecognizerState.Ended {
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                
+                
+                //Pan to right, animate offscreen to right
+                if translation.x >= 50 {
+                    self.cardView.center.x = self.swipedRightPosition
+                }
+                    
+                //Pan to left, animate offscreen to left
+                if translation.x <= -50 {
+                    self.cardView.center.x = self.swipedLeftPosition
+                }
+                
+                //Pan in each direction
+                else {
+                    self.cardView.center.x = self.snappedBackPosition
+                    
+                    
+                }
+
+                
+        })
+    }
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
