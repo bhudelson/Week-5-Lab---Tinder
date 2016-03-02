e5235775f1f90ecba141fc49dc05389e3e9e5e9f
bhudelson <bhudelso@gmail.com>
2016-03-01 22:19:51 -0800

    Added Assets

---
+++ Week 5 Lab - Tinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week 5 Lab - Tinder
+//
+//  Created by Blake Hudelson on 2/29/16.
+//  Copyright © 2016 Blake Hudelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
